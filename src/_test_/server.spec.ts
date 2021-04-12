658b810a360e903bad43eb1976190959993b1896
Luis Angel Armenta <luigiangelxd@gmail.com>
2021-04-11 20:42:08 -0500

    feat: server created

---
+++ src/_test_/server.spec.ts
@@ -0,0 +1,24 @@
+/* eslint-disable @typescript-eslint/no-floating-promises */
+import request from 'supertest'
+import { Server as IServer } from 'node:http'
+import { server as myServer } from '../server'
+
+describe('loading express', () => {
+  let server: IServer
+  beforeEach(() => {
+    server = myServer
+  })
+  afterEach(() => {
+    server.close()
+  })
+  it('responds to /', function testSlash (done) {
+    request(server)
+      .get('/')
+      .expect(200, done)
+  })
+  it('404 everything else', function testPath (done) {
+    request(server)
+      .get('/foo/bar')
+      .expect(404, done)
+  })
+})
