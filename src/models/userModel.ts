368e07084a10185bfca1f32cc95a06004c2f3b55
Luis Angel Armenta <luigiangelxd@gmail.com>
2021-04-12 15:37:48 -0500

    fix: User schema fixed

--- src/models/userModel.ts
+++ src/models/userModel.ts
@@ -1,9 +1,9 @@
 import { model, Schema } from 'mongoose'
 
 const userSchema = new Schema({
-  name: { type: String, required: 'username is required' },
-  email: { type: String, required: true, unique: 'email already exists', index: true, match: [/. +@. +\.. + /, 'Please give a valid email address'] },
-  password: { type: String, required: 'password is required' },
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
   hasMotorcycle: { type: Boolean, default: false }
 })
 
