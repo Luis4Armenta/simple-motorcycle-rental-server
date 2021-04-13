e040aed875efd03ad509e636d87409b91a841efd
Luis Angel <beku186@gmail.com>
2021-04-13 18:32:38 -0500

    refactor: Unnecessary logs removed

--- src/middlewares/auth.middleware.ts
+++ src/middlewares/auth.middleware.ts
@@ -5,26 +5,20 @@ import getToken from '../utils/helper'
 export function verifyToken (request: Request | any, response: Response, next: NextFunction): any {
   try {
     const header = request.headers.authorization
-    console.log(header, 'header')
     // eslint-disable-next-line @typescript-eslint/strict-boolean-expressions
     if (!header) {
-      console.log('no está el header')
       return response.status(401).send('deauthenticate request')
     }
 
     const token = getToken(header)
-    console.log(token, 'token solo')
     if (token === null) {
-      console.log('existe el header, pero no está el token en segunda posición')
       return response.status(401).send('deauthenticate request')
     }
 
     const payload: any = new JsonWebToken().verify(token)
-    console.log(payload, 'contenido de payload')
     request.userId = payload.id
     return next()
   } catch (error) {
-    console.log('hubo un error', error)
     return response.status(401).send('deauthenticate request')
   }
 }
