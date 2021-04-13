5eb95900aee20d5b5f52611f9ebfecb5aa63bb01
Luis Angel <beku186@gmail.com>
2021-04-13 18:38:46 -0500

    fix: added authorization middleware support

--- src/useCases/hasMotocycle/hasMotocycleController.ts
+++ src/useCases/hasMotocycle/hasMotocycleController.ts
@@ -1,19 +1,12 @@
 import { Request, Response } from 'express'
-import getToken from '../../utils/helper'
 import { HasMotorcycleUseCase } from './hasMotocycle.useCase'
 
 export class HasMotorcycleController {
   constructor (private readonly hasMotorcycleUseCase: HasMotorcycleUseCase) { }
 
-  async handle (request: Request, response: Response): Promise<Response> {
-    const header = request.headers.authorization
-    // eslint-disable-next-line @typescript-eslint/strict-boolean-expressions
-    if (header) {
-      return await this.hasMotorcycleUseCase.execute(getToken(header))
-        .then((status) => response.send(status))
-        .catch(() => response.send(false))
-    } else {
-      return response.send(false)
-    }
+  async handle (request: Request | any, response: Response): Promise<Response> {
+    return await this.hasMotorcycleUseCase.execute(request.userId)
+      .then((status) => response.status(200).send(status))
+      .catch(() => response.status(200).send(false))
   }
 }
--- src/useCases/takeMotorcycle/takeMotorcycleController.ts
+++ src/useCases/takeMotorcycle/takeMotorcycleController.ts
@@ -1,18 +1,11 @@
 import { Request, Response } from 'express'
-import getToken from '../../utils/helper'
 import { TakeMotorcycleUseCase } from './takeMotorCycle.useCase'
 
 export class TakeMotorcycleController {
   constructor (private readonly takeMotorcycleUseCase: TakeMotorcycleUseCase) { }
-  async handle (request: Request, response: Response): Promise<Response> {
-    const header = request.headers.authorization
-    // eslint-disable-next-line @typescript-eslint/strict-boolean-expressions
-    if (header) {
-      return await this.takeMotorcycleUseCase.execute(getToken(header), parseInt(request.body.motorcycleNumber))
-        .then((status) => response.send(status))
-        .catch(() => response.send(false))
-    } else {
-      return response.send(false)
-    }
+  async handle (request: Request | any, response: Response): Promise<Response> {
+    return await this.takeMotorcycleUseCase.execute(request.userId, parseInt(request.body.motorcycleNumber))
+      .then((status) => response.send(status))
+      .catch(() => response.send(false))
   }
 }
