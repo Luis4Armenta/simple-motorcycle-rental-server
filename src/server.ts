d6ac938e94f1e4ba0d54f128e8b1eea0167b4f01
beku86 <luigiangelxd@gmail.com>
2021-04-14 15:57:15 -0500

    feat: adding sockets to the project

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,8 @@
 /* eslint-disable @typescript-eslint/no-floating-promises */
 import mongoose from 'mongoose'
 import { app } from './app'
+// import socketIo from 'socket.io'
+import { Server, Socket } from 'socket.io'
 
 mongoose.connect('mongodb://localhost/motorcycles', { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: true, useCreateIndex: true })
 const db = mongoose.connection
@@ -11,6 +13,11 @@ db.once('open', () => {
 
 const server = app.listen(3000, () => {
   console.log('server running..')
+})
+
+const io = new Server(server)
+io.on('connection', (socket: Socket) => {
+  console.log('new Connection')
 })
 
 export { server }
