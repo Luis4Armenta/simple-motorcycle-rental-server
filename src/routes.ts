e70a9cb82fbb66339514251690b247750b3c8e07
Luis Angel <beku186@gmail.com>
2021-04-14 00:48:51 -0500

    feat: output created for our new features

--- src/routes.ts
+++ src/routes.ts
@@ -1,7 +1,10 @@
 import { Router } from 'express'
 import { verifyToken } from './middlewares/auth.middleware'
 import { createUserController } from './useCases/createUser'
+import { getAllMotorcyclesController } from './useCases/getAllMotorcycles'
+import { getAvailableMotorcycleController } from './useCases/getAvailableMotorcycles'
 import { getMotorycycleNumberController } from './useCases/getMotorcycleNumber'
+import { getQuantityMotorcycleController } from './useCases/getQuantityMotorcycles'
 import { hasMotorcycleController } from './useCases/hasMotocycle'
 import { loginUserController } from './useCases/loginUser'
 import { returnMotorcycleController } from './useCases/returnMotorcycle'
@@ -31,6 +34,18 @@ router.get('/motorcycle/return', verifyToken, (request, response): any => {
 
 router.get('/motorcycle/number', verifyToken, (request, response): any => {
   return getMotorycycleNumberController.handle(request, response)
+})
+
+router.get('/motorcycle/motorcycles', verifyToken, (request, response): any => {
+  return getAllMotorcyclesController.handle(request, response)
+})
+
+router.get('/motorcycle/quantity', verifyToken, (request, response): any => {
+  return getQuantityMotorcycleController.handle(request, response)
+})
+
+router.get('/motorcycle/available', verifyToken, (request, response): any => {
+  return getAvailableMotorcycleController.handle(request, response)
 })
 
 export { router }
