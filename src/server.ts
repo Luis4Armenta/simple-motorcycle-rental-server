118edcd5583a81dd121c1a5c16e175514b2c52ca
beku86 <luigiangelxd@gmail.com>
2021-04-15 20:40:48 -0500

    bad socket.Io implementation fixed

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,13 @@
+/* eslint-disable @typescript-eslint/no-misused-promises */
+/* eslint-disable @typescript-eslint/strict-boolean-expressions */
 /* eslint-disable @typescript-eslint/no-floating-promises */
+import mongoose from 'mongoose'
+import jwt from 'jsonwebtoken'
+import { Server as SocketServer, Socket } from 'socket.io'
 import { Server } from 'http'
 import { app } from './app'
-import { Server as SocketServer, Socket } from 'socket.io'
-import mongoose from 'mongoose'
 
-// mongoose connecion
-mongoose.connect('mongodb://localhost/motorcycles', { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: true, useCreateIndex: true })
+mongoose.connect('mongodb://localhost/motorcycles', { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
 const db = mongoose.connection
 db.on('error', console.error.bind(console, 'connection error:'))
 db.once('open', () => {
@@ -22,12 +24,23 @@ const io = new SocketServer(httpServer, {
   }
 })
 
-io.on('connection', (socket: Socket) => {
-  console.log('connection')
+io.use((socket: Socket | any, next) => {
+  if (socket.handshake.query?.token) {
+    if (typeof (socket.handshake.query.token) === 'string') {
+      jwt.verify(socket.handshake.query.token, 'secretWord', (err: any, decoded: any) => {
+        if (err) Error('the token could not be decoded')
+        socket.decoded = decoded.id
+        next()
+      })
+    } else {
+      console.log('The token is not what was expected')
+      next(new Error('Authentication error'))
+    }
+  }
 })
 
 httpServer.listen(3000, () => {
-  console.log('Open Server')
+  console.log('the server is running!..')
 })
 
 export { httpServer }
