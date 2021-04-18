65cc1c9305c42a7586ea4ecd3d8f058440adb546
beku86 <luigiangelxd@gmail.com>
2021-04-18 18:46:19 -0500

    test: description correction

--- src/_test_/bcryptEncryptor.spec.ts
+++ src/_test_/bcryptEncryptor.spec.ts
@@ -3,7 +3,7 @@ import { BcryptEncryptor } from '../providers/implementations/BcryptEncryptor'
 describe('Encryptor', () => {
   const encryper = new BcryptEncryptor()
 
-  test('encrypt', () => {
+  test('An encrypted text should give true if it is compared with an equal one', () => {
     const sut = encryper.encrypt('hello')
     expect(encryper.compare('hello', sut)).toBeTruthy()
   }
