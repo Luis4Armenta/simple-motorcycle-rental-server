c915f2f8ce3e2811e57f2dfcf2d4d5fb6260bc42
beku86 <luigiangelxd@gmail.com>
2021-04-19 00:19:34 -0500

    fix: params error fixed

--- src/useCases/createUser/index.ts
+++ src/useCases/createUser/index.ts
@@ -1,10 +1,12 @@
+import { BcryptEncryptor } from '../../providers/implementations/BcryptEncryptor'
 import { MongodbUserRepository } from '../../repositories/implementations/MongodbUserRepository'
 import { CreateUserUseCase } from './createUser.useCase'
 import { CreateUserController } from './createUserController'
 
 const mongoDBUserRepository = new MongodbUserRepository()
+const ecryptor = new BcryptEncryptor()
 
-const createUserUseCase = new CreateUserUseCase(mongoDBUserRepository)
+const createUserUseCase = new CreateUserUseCase(mongoDBUserRepository, ecryptor)
 const createUserController = new CreateUserController(createUserUseCase)
 
 export { createUserController }
